38160d63312b25e2b474ee03753500eb1be71ed3
harikishorerayala <harikishorerayala@gmail.com>
2021-01-06 18:16:05 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //property
   title = 'blog';
+  //function
+  getTitle(){
+    return this.title;
+  }
+  //object
+  obj={
+    name:"Hari",
+    age:"35"
+  };
+  //array
+  arr=["hari","kishore","rayala"];
+
+  objWindow=window;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'
 import { NgModule } from '@angular/core';
+
+import {UsersModule} from './users/users.module'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsersModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
